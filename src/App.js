1fea6cbee7bba83deeebf500f97dca8f5f133b1a
Holzlowski <43970216+Holzlowski@users.noreply.github.com>
2025-07-25 16:32:23 +0200

    Add initial production build and assets
    Adds the compiled build output for the React app, including HTML, manifest, icons, static assets, and media files. Also updates dependencies in package.json and package-lock.json, and modifies src/App.js. This prepares the project for deployment.

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import './App.css';
 function App() {
   return (
     <>
-      <Router>
+      <Router basename="/Meine-Webseite">
         {/* Navigation */}
         <NavigationBar className="fixed-navbar" />
         <Routes>
