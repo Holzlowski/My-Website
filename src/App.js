ed869031ff31ff3960105760ef5bac022c99949c
Holzlowski <43970216+Holzlowski@users.noreply.github.com>
2026-05-06 16:55:12 +0200

    Reset scroll on route changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import {
   NavigationBar,
   SocialSidebar,
@@ -10,10 +11,22 @@ import WestriaPage from './pages/Westria/WestriaPage';
 import PortfolioPage from './pages/PortfolioPage';
 import './App.css';
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'auto' });
+  }, [pathname]);
+
+  return null;
+}
+
 function App() {
   return (
     <>
       <Router basename="/My-Website">
+        <ScrollToTop />
+
         {/* Navigation */}
         <NavigationBar className="fixed-navbar" />
         
